3b81a32b68490b7ca3b0c2e7366b9bd760ef0605
eMobilis <b3tt99@gmail.com>
2023-03-15 08:56:11 +0300

    Area, BMI and Datatypes

---
+++ src/main/kotlin/BMI.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var weight = 85
+    var height = 1.5
+
+    var square = height * height
+    var BMI = weight / square
+
+    print(BMI)
+}
---
+++ src/main/kotlin/area.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var length = 15
+    var width = 26
+
+    var area  = length * width
+
+    print(area)
+
+}
